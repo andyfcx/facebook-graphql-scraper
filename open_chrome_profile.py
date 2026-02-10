f2d4234f6684b0d8d92a14e81f9900cc19d5c2d2
andyfcx <dreamcastle.andy@gmail.com>
2026-02-10 13:31:44 +0800

    Read from .env

--- open_chrome_profile.py
+++ open_chrome_profile.py
@@ -2,15 +2,27 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
+
+user_data_dir = os.getenv("CHROME_USER_DATA_DIR")
+chromedriver_path = os.getenv("CHROMEDRIVER_PATH")
+debugger_address = os.getenv("CHROME_DEBUGGER_ADDRESS")
+profile_dir = os.getenv("CHROME_PROFILE_DIR", "Default")
+log_path = os.getenv("CHROMEDRIVER_LOG_PATH", "./tmp/chromedriver.log")
+
+if not user_data_dir:
+    raise ValueError("CHROME_USER_DATA_DIR is required in .env")
+if not chromedriver_path:
+    raise ValueError("CHROMEDRIVER_PATH is required in .env")
 
 chrome_options = Options()
 chrome_options.add_argument("--remote-debugging-pipe")
 chrome_options.add_argument(
-    "--user-data-dir=/Users/y/chrome-fb-user-data"
+    f"--user-data-dir={user_data_dir}"
 )
-chrome_options.add_argument("--profile-directory=Default")
+chrome_options.add_argument(f"--profile-directory={profile_dir}")
 chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 chrome_options.add_experimental_option(
     "excludeSwitches", ["enable-automation"]
@@ -18,11 +30,17 @@ chrome_options.add_experimental_option(
 chrome_options.add_experimental_option(
     "useAutomationExtension", False
 )
+chrome_options.add_experimental_option("detach", True)
 chrome_options.add_argument("--no-first-run")
 chrome_options.add_argument("--no-default-browser-check")
 
-service = Service("/Users/y/facebook-graphql-scraper/drivers/chromedriver-mac-x64/chromedriver",
-            service_args=["--verbose", "--log-path=./tmp/chromedriver.log"])
+if debugger_address:
+    chrome_options.add_experimental_option("debuggerAddress", debugger_address)
+
+service = Service(
+    chromedriver_path,
+    service_args=["--verbose", f"--log-path={log_path}"],
+)
 
 driver = webdriver.Chrome(
     service=service,
@@ -30,3 +48,6 @@ driver = webdriver.Chrome(
 )
 
 driver.get("https://www.facebook.com")
+
+# Detach from the session without closing the browser.
+driver.quit()
